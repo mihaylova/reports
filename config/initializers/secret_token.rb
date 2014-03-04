e1bb442f3ec563886fdbcab009a07efd8fcdf6f2
Inna Mihailova <innna.p.m@gmail.com>
2014-03-04 13:45:21 +0200

    Initial commit and use devise for users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MelonCourse::Application.config.secret_key_base = '398d598c953947af6f258136d244a14725920cfbe357695c034602b25b180077541ef1c2cf81db52a05098a29090ea02e03bee3e69fb8d674679c68e2d0dce8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MelonCourse::Application.config.session_store :cookie_store, key: '_melon_course_session'
