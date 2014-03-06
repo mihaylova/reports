e365105c0878b4281c5399f69fab41ee71188a99
Inna Mihailova <innna.p.m@gmail.com>
2014-03-06 14:34:16 +0200

    Add new validations to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,8 @@ class Report < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   
-  validates :text, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
   validates :user, presence: true
   validates :category, presence: true
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Report do
   it { should belong_to(:user) }
   it { should belong_to(:category) }
-  it { should validate_presence_of(:text) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:category) }
 end
