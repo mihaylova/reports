8c3ddcad7160de6b64126d5005448dc01fd1eb80
Inna Mihailova <innna.p.m@gmail.com>
2014-03-10 21:57:46 +0200

    Administration: alter category index page and filters

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,17 +1,17 @@
 ActiveAdmin.register Category do
 menu priority: 5
   
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+   index do
+    selectable_column
+
+    column("Name", nil, sortable: :name) {|category| link_to category.name, admin_category_path(category)}
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :name
+  filter :created_at
+  config.per_page = 10
   
 end
