e8cf722f1caf08b81a91360b402745563014188d
Inna Mihailova <innna.p.m@gmail.com>
2014-03-10 21:46:22 +0200

    Change user can can abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,16 +6,30 @@ class Ability
     #
        user ||= User.new # guest user (not logged in)
        alias_action :create, :read, :update, :destroy, :to => :crud
+       can :read, :all
 
-
-       if user.new_record?
+      if user.editor?
+        can :create, :all
         can :read, :all
-      elsif user.admin?
-        can :manage, :all
+        can :update, Report, permissions: {editor_can_edit: true}
+        can :destroy, Report, permissions: {editor_can_delete: true}
+        can :update, Report, permissions: {author_can_edit: true}, user_id: user.id
+        can :destroy, Report, permissions: {author_can_delete: true}, user_id: user.id
       else
-        can :manage, Report, user_id: user.id
+        can :create, :all
         can :read, :all
+        can :update, Report, permissions: {author_can_edit: true}, user_id: user.id
+        can :destroy, Report, permissions: {author_can_delete: true}, user_id: user.id
       end
+
+      #  if user.new_record?
+      #   can :read, :all
+      # elsif user.editor?
+      #   can :manage, :all
+      # else
+      #   can :manage, Report, user_id: user.id
+      #   can :read, :all
+      # end
         
     #
     # The first argument to `can` is the action you are giving the user 
