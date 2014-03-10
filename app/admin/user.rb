7e32f536801c809685cf290ba6b96a572ac7e51b
Inna Mihailova <innna.p.m@gmail.com>
2014-03-10 11:28:53 +0200

    Change admin/user index, show and edit design

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,4 +4,56 @@ ActiveAdmin.register User do
   menu :label => "Authors"
   menu :parent => "Users"
 
+  index do
+    selectable_column
+
+    column("Name", nil, sortable: :name) {|user| link_to user.name, admin_user_path(user)}
+    column :email
+    column :admin
+    column :created_at
+    column :current_sign_in_at
+    column :current_sign_in_ip
+    column :sign_in_count
+    actions
+  end
+  
+
+  show do |user|
+    panel "Account Details" do
+      attributes_table_for user  do
+        row :name
+        row :email
+        row :role
+        row :created_at
+        row :updated_at
+      end
+    end
+
+    panel "Login Details" do
+      attributes_table_for user  do
+        row("current singed in at") {|user| user.current_sign_in_at || "Never signed in"}
+        row("current singed in ip") {|user| user.current_sign_in_ip || "Never signed in"}
+        row("last singed in at") {|user| user.last_sign_in_at || "Never signed in"}
+        row("last singed in ip") {|user| user.last_sign_in_ip || "Never signed in"}
+        row :sign_in_count
+      end
+    end
+    active_admin_comments
+  end
+
+   form do |f|
+      f.semantic_errors *f.object.errors.keys
+      f.inputs "Details" do
+        f.input :name
+        f.input :email
+      end
+      f.inputs "Change Password" do
+        f.input :password
+        f.input :password_confirmation
+      end
+      
+      f.actions
+    end
+
+
 end
