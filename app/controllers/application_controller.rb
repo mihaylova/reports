e52d019fccc6a182691a93b865407ebba904b44c
Inna Mihailova <innna.p.m@gmail.com>
2014-03-14 17:06:11 +0200

    Add name field in user's registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,15 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to new_user_session_path, :alert => "exception.message"
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(
+          :email, :password, :password_confirmation, :name
+        )
+      end
+    end
 end
