961bba526512c5ac838f72b9717fea5355378a48
Inna Mihailova <innna.p.m@gmail.com>
2014-03-14 16:50:26 +0200

    Fix bug with permissions

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, only: [:new, :create]
   load_and_authorize_resource skip_load_resource only: [:create] 
 
 
