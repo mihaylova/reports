9e8e3e1324924181ddbd454fa57121a53731f1e9
Inna Mihailova <innna.p.m@gmail.com>
2014-03-17 10:34:41 +0200

    Add default picture to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 
   after_update :send_notification
 
-  has_attached_file :image, :styles => {:small => "50x50#", large: "700x700" }
+  has_attached_file :image, :styles => {:small => "50x50#", large: "700x700" }, :default_url => "/images/:style/missing.jpeg"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
